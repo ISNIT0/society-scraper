8d39b4852e63d0464684b970d917bd70d68af0c4
Kai <Kai@Kais-MacBook-Pro-4.local>
2019-10-18 15:20:54 +0100

    removed ucl

--- src/societies/index.ts
+++ src/societies/index.ts
@@ -2,10 +2,6 @@ export * from './CUSU';
 export * from './OX';
 export * from './NUSU';
 export * from './StAndrews';
-<<<<<<< HEAD
 export * from './LSE';
 export * from './Imperial';
 export * from './Bath';
-=======
-export * from './UCL';
->>>>>>> eda6ab184057ec69ad3e32df85d6a87e931bc84c
