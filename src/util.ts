c9f86bc95fec87ed4239e728106870d3e96a4ee4
Joe Reeve <joseph.reeve@googlemail.com>
2019-10-29 09:52:31 +0000

    :construction_worker: minor tidy up and add basic tagging

--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,15 @@
 import { SocietyData, SocietyScraper } from "./SocietyScraper";
 import { resolve } from "url";
+import tagMappings from './tagMappings.json';
 
 export function postProcessData(scraper: SocietyScraper, data: SocietyData): SocietyData {
     return Object.entries<string>(data)
         .reduce((acc, [key, value]) => {
-            let val = value.trim();
-            if ((val || '').startsWith('/')) {
-                val = resolve(scraper.entryUrl, val);
+            if (typeof value === 'string') {
+                let val = value.trim();
+                if ((val || '').startsWith('/')) {
+                    val = resolve(scraper.entryUrl, val);
+                }
             }
 
             return {
@@ -18,4 +21,14 @@ export function postProcessData(scraper: SocietyScraper, data: SocietyData): Soc
 
 export function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const tags = Object.keys(tagMappings);
+export function getTags(_text: string) {
+    const text = _text.toLocaleLowerCase().replace(/[^A-z0-9]/g, '');
+    const applicableTags = tags.filter((t) => {
+        const tagKeys = (tagMappings as any)[t];
+        return tagKeys.some((tk: string) => text.includes(tk));
+    });
+    return applicableTags;
 }
